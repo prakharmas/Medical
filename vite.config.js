bbee2da1c73dbfa435e8656199e6b21304e95140
Prakhar Gupta <Prakhar.gupta@teammas.co.in>
2026-08-17 13:25:06 +0530

    ssl

--- vite.config.js
+++ vite.config.js
@@ -1,1 +1,15 @@
-import {defineConfig} from 'vite';import react from '@vitejs/plugin-react';export default defineConfig({plugins:[react()]});
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import fs from 'fs';
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    https: process.env.SSL_KEY_FILE && process.env.SSL_CERT_FILE
+      ? {
+          key: fs.readFileSync(process.env.SSL_KEY_FILE),
+          cert: fs.readFileSync(process.env.SSL_CERT_FILE),
+        }
+      : false,
+  },
+});
